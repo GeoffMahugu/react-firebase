fbc9873ec7dc0ede10ad66c7afbf971d271b4f7a
GeoffMahugu <geoffreymahugu@gmail.com>
2021-05-20 19:02:51 +0300

    Current status

--- src/pages/CartPage.tsx
+++ src/pages/CartPage.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { auth } from '../config/firebase';
 import IPageProps from '../interfaces/page.interface';
 
 const CartPage: React.FunctionComponent<IPageProps> = props => {
+    // Logs the current user
+    console.log(auth.currentUser)
     return (
         <div>
             <h1> Cart Page - Protected</h1>
