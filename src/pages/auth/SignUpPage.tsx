7b13e0cfa09fd63dd0375b988d0e0d609ee614c2
GeoffMahugu <geoffreymahugu@gmail.com>
2021-05-20 14:30:02 +0300

    Updated signup page

--- src/pages/auth/SignUpPage.tsx
+++ src/pages/auth/SignUpPage.tsx
@@ -1,10 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react'
+import { Link, useHistory } from 'react-router-dom';
+import firebase from 'firebase';
+
 import IPageProps from '../../interfaces/page.interface';
+import { SignInWithSocialMedia } from '../../modules/auth';
+import { Providers } from '../../config/firebase';
 
 const SignUpPage: React.FunctionComponent<IPageProps> = props => {
+    const [authenticating, setAuthenticating] = useState<boolean>(false);
+    const [error, setError] = useState<string>('');
+    const history = useHistory();
+
+    const signInWithSocialMedia = (provider: firebase.auth.AuthProvider) => {
+        if (error !== '') setError('');
+
+        setAuthenticating(true);
+
+        SignInWithSocialMedia(provider)
+            .then(result => {
+                history.push('/');
+            })
+            .catch(error => {
+                setAuthenticating(false);
+                setError(error.message);
+            });
+    }
+
     return (
-        <div>
-            Login Page - Unprotected
+        <div className="AuthLogin">
+            <div className="auth-main-container">
+                <div>
+                    <h1 >Welcome to React App</h1>
+                    <p >Please Signup to continue by choosing one of the options below.</p>
+                </div>
+                <div className="auth-btn-wrapper">
+                    <button
+                        disabled={authenticating}
+                        onClick={() => signInWithSocialMedia(Providers.google)}
+                    >
+                        SignUp with Google</button>
+                    <button
+                        disabled={authenticating}
+                        onClick={() => signInWithSocialMedia(Providers.facebook)}
+                    >
+                        Sign with Facebook</button>
+
+                    <Link to={`/`}>
+                        <button>Back To Home Page</button>
+                    </Link>
+                </div>
+            </div>
         </div>
     );
 }
